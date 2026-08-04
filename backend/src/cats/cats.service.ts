ee600388126266210481de73b048a3afcd6c75b3
Wendy Shen <wendyshen2005@gmail.com>
2026-08-04 10:24:46 -0400

    hey

--- backend/src/cats/cats.service.ts
+++ backend/src/cats/cats.service.ts
@@ -4,40 +4,40 @@ import { Injectable } from '@nestjs/common';
 export class CatsService {
     getMeow(): string {
         let random = Math.floor(Math.random()*10);
-        let res = "";
+        let res = `${random}`;
         switch(random) {
             case 0:
-                res = "Meow, world";
+                res += "Meow, world";
                 break;
             case 1:
-                res = "meow world :(";
+                res += "meow world :(";
                 break;
             case 2:
-                res = "now world?????"
+                res += "now world?????"
                 break;
             case 3:
-                res = "go kys!!!! (in minecraft)"
+                res += "go kys!!!! (in minecraft)"
                 break;
             case 4:
-                res = "do not kys!!!! (irl)"
+                res += "do not kys!!!! (irl)"
                 break;
             case 5:
-                res = "hey hey hey";
+                res += "hey hey hey";
                 break;
             case 6:
-                res = "Hello world :)";
+                res += "Hello world :)";
                 break;
             case 7:
-                res = "Bark bark bark (delicious)";
+                res += "Bark bark bark (delicious)";
                 break;
             case 8:
-                res = "NOOOOOOOOOOOOOOOOOOOO";
+                res += "NOOOOOOOOOOOOOOOOOOOO";
                 break;
             case 9:
-                res = "water your petels :/";
+                res += "water your petels :/";
                 break;
             default:
-                res = "HAIIIIIIIIIIIIII";
+                res += "HAIIIIIIIIIIIIII";
                 break;
         }
         return res;
